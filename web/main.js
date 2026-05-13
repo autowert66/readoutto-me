10fd2e028c1adb4177c38a76122a9e968023ad56
Gabriel <72566626+autowert66@users.noreply.github.com>
2026-05-13 23:56:20 +0200

    Add dynamic theme switching helper to handle color theme changes while the page is open This also makes debugging easier since the emulate dark/light mode buttons in the dev-tools works now

--- web/main.js
+++ web/main.js
@@ -1,6 +1,9 @@
 const playAudioBtn = document.getElementById('playAudio');
 let audioEl;
 
+// dynamically import dynamic-theme in a non-blocking way since it is not critical (promise is not awaited)
+import('./utils/dynamic-theme.js');
+
 playAudioBtn.addEventListener('click', (ev) => {
   ev.preventDefault();
 
---
+++ web/utils/dynamic-theme.js
@@ -0,0 +1,6 @@
+// beer css supports light/dark mode and switches automatically, but does not handle switching while on the page
+// this helper listens for changes to the media query and forces it to apply the changes instantly
+if ('matchMedia' in window) {
+  const query = window.matchMedia('(prefers-color-scheme: dark)');
+  query.addEventListener('change', () => window?.ui('mode', 'auto'));
+}
