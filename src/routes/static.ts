28c9db94639714260b53e36acffa8e1ff1a58b80
Gabriel <72566626+autowert66@users.noreply.github.com>
2026-05-19 11:28:11 +0200

    Add preconnect for font and aggresive caching for vendor assets

--- src/routes/static.ts
+++ src/routes/static.ts
@@ -3,6 +3,12 @@ import { serveStatic } from 'hono/deno';
 
 const staticRoute = new Hono();
 
+// aggresive caching for /vendor/ requests
+staticRoute.use('/vendor/*', async (c, next) => {
+  c.header('Cache-Control', 'public, max-age=31536000, immutable');
+  await next();
+});
+
 staticRoute.use('/*', serveStatic({ root: './public' }));
 staticRoute.use('/*', serveStatic({ root: './web' }));
 
