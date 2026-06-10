287c5b580cc5cc73c73f3e4b37990b8fbc3920ec
Gabriel <72566626+autowert66@users.noreply.github.com>
2026-06-10 13:39:15 +0200

    Move frontend typescript code into web/src/

--- web/src/utils/client.ts
+++ web/src/utils/client.ts
@@ -1,4 +1,4 @@
 import { hc } from 'hono/client';
-import type { AppType } from '../../src/app.ts';
+import type { AppType } from '../../../src/app.ts';
 
 export const client = hc<AppType>(location.origin);
