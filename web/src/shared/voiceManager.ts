bcc1674758ae27164dbe645d558beef898badc82
Gabriel <72566626+autowert66@users.noreply.github.com>
2026-07-10 16:20:14 +0200

    Add error logging if loading voices fails

--- web/src/shared/voiceManager.ts
+++ web/src/shared/voiceManager.ts
@@ -1,5 +1,13 @@
 import { getVoices } from '../utils/getVoices.ts';
+import { showSnackbar } from '../utils/showSnackbar.ts';
 import { VoiceManager } from '../utils/VoiceManager.ts';
 
 export const voiceManagerPromise = getVoices()
-  .then((voices) => new VoiceManager(voices));
+  .then((voices) => new VoiceManager(voices))
+  .catch((err) => {
+    console.error('Loading voices failed:', err);
+    showSnackbar('Failed to load voices, please reload the page', 'error');
+
+    // propagate the error, otherwise the voiceManagerPromise.then would be called with undefined
+    throw err;
+  });
